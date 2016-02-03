3a1eb4c4f62c6b6de131188ff5eeb79da5cd2992
Tsur <prucheta@gmail.com>
2016-02-03 22:36:21 +0100

    [FEAT] added basic stuff

---
+++ js/index.js
@@ -0,0 +1,119 @@
+var range_els = document.querySelectorAll('[class*=webkit] input[type=range]'),
+    n_els, style_el, styles, units, base_sel, track_sel, _thumb_sel, a, b;
+
+if(range_els) {
+  style_el = document.createElement('style');
+  document.body.appendChild(style_el);
+
+  n_els = range_els.length;
+
+  styles = [];
+  units = ['Parrafos', ['MB', 'GB'], 'GB'];
+
+  base_sel = '.js input[type=range]';
+  track_sel = ['::-webkit-slider-runnable-track', ' /deep/ #track'];
+  thumb_sel = ['::-webkit-slider-thumb', ' /deep/ #thumb'];
+  a = ':after';
+  b = ':before';
+
+  for(var i = 0; i < n_els; i++) {
+    styles.push('');
+
+    range_els[i].addEventListener('input', function() {
+      var idx = ~~this.id.split('r')[1],
+          min = this.min || 0,
+          max = this.max || 100,
+          perc = ~~(100*(this.value - min)/(max - min)),
+          val = ~~this.value,
+          u = units[idx - 1],
+          curr_sel = base_sel + '[id=r' + idx + ']',
+          str = '';
+
+      if(idx == 2) {
+        u = u[(val < 3)?0:1];
+        val = (val < 3) ? Math.pow(2, 7 + val) : (val - 2);
+      }
+
+      str += curr_sel + track_sel[0] + '{background-size:' + perc + '% 100%}';
+      str += curr_sel + thumb_sel[0] + b + ',' + curr_sel + thumb_sel[1] + b +
+        '{content:"' + val + '"}';
+      str += curr_sel + thumb_sel[0] + a + ',' + curr_sel + thumb_sel[1] + a +
+        '{content:"' + u + '"}';
+
+      styles[idx] = str;
+
+      style_el.textContent = styles.join('');
+    }, false);
+  }
+}
+
+function getRandomInt(min, max) {
+    var rand = Math.floor(Math.random() * (max + 1 - min) + min);
+    return rand;
+}
+
+function generateFacts(facts){
+
+  var paragraph = '';
+
+  for (var i = 0; i < 8; i++) {
+      if (i === 7) {
+          paragraph += facts[getRandomInt(0, facts.length - 1)] + '.';
+      } else {
+          paragraph += facts[getRandomInt(0, facts.length - 1)] + ', ';
+      }
+  }
+
+  return paragraph;
+
+}
+
+function buildParagraphs(paragraphsNumber, data){
+
+  var factsContainer = document.querySelector('.facts')
+
+  while (factsContainer.firstChild) {
+      factsContainer.removeChild(factsContainer.firstChild);
+  }
+
+  for(var i=0; i<paragraphsNumber; i++){
+
+    var p = document.createElement('p');
+
+    p.classList.add('fact')
+
+    p.textContent = generateFacts(data.spanish);
+
+    factsContainer.appendChild(p);
+
+  }
+
+}
+
+function generateLorem(paragraphsNumber) {
+
+    var request = new XMLHttpRequest();
+    request.open('GET', 'facts.json', true);
+
+    request.onload = function () {
+        if (request.status >= 200 && request.status < 400) {
+            // Success
+            buildParagraphs(paragraphsNumber, JSON.parse(request.responseText));
+        } else {
+            console.error('Error ' + request.status);
+        }
+    };
+
+    request.onerror = function () {
+        // There was a connection error of some sort
+        console.error('Connection Error');
+    };
+    request.send();
+}
+
+
+document.querySelector('#r1').addEventListener('change', function(e){
+
+  generateLorem(e.target.value);
+
+});
